61c118cd41d28c38cd045724a4a8e811ee6b9fa3
shivamsehgal77 <112571645+shivamsehgal77@users.noreply.github.com>
2023-11-14 12:11:40 -0500

    created new branch ros_services_logging_launch

--- src/publisher_member_function.cpp
+++ src/publisher_member_function.cpp
@@ -13,17 +13,12 @@
 // limitations under the License.
 
 #include <chrono>
-#include <functional>
 #include <memory>
-#include <string>
 
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
+#include "tutorial_interfaces/msg/num.hpp"                                            // CHANGE
 
 using namespace std::chrono_literals;
-
-/* This example creates a subclass of Node and uses std::bind() to register a
- * member function as a callback from the timer. */
 
 class MinimalPublisher : public rclcpp::Node
 {
@@ -31,7 +26,7 @@ public:
   MinimalPublisher()
   : Node("minimal_publisher"), count_(0)
   {
-    publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+    publisher_ = this->create_publisher<tutorial_interfaces::msg::Num>("topic", 10);  // CHANGE
     timer_ = this->create_wall_timer(
       500ms, std::bind(&MinimalPublisher::timer_callback, this));
   }
@@ -39,13 +34,13 @@ public:
 private:
   void timer_callback()
   {
-    auto message = std_msgs::msg::String();
-    message.data = "Hi, I am Shivam Sehgal! " + std::to_string(count_++);
-    RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+    auto message = tutorial_interfaces::msg::Num();                                   // CHANGE
+    message.num = this->count_++;                                                     // CHANGE
+    RCLCPP_INFO_STREAM(this->get_logger(), "Publishing: '" << message.num << "'");    // CHANGE
     publisher_->publish(message);
   }
   rclcpp::TimerBase::SharedPtr timer_;
-  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+  rclcpp::Publisher<tutorial_interfaces::msg::Num>::SharedPtr publisher_;             // CHANGE
   size_t count_;
 };
 
