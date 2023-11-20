0afbc7005494676708c199b8b7eece356e8c4e8c
shivamsehgal77 <112571645+shivamsehgal77@users.noreply.github.com>
2023-11-19 19:20:13 -0500

    1.) Made new branch for ROS2 Assignment 2 2.) Added ROS service to the publisher

--- src/publisher_member_function.cpp
+++ src/publisher_member_function.cpp
@@ -19,9 +19,11 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
+#include "cpp_pubsub/srv/modify_message.hpp"
 
 using namespace std::chrono_literals;
-
+using std::placeholders::_1;
+using std::placeholders::_2;
 /* This example creates a subclass of Node and uses std::bind() to register a
  * member function as a callback from the timer. */
 
@@ -31,9 +33,31 @@ public:
   MinimalPublisher()
   : Node("minimal_publisher"), count_(0)
   {
+    this->declare_parameter<double>("publish_frequency", 2.0);  // Default frequency: 2.0 Hz
+    double publish_frequency = this->get_parameter("publish_frequency").as_double();
+
     publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
     timer_ = this->create_wall_timer(
-      500ms, std::bind(&MinimalPublisher::timer_callback, this));
+      std::chrono::duration<double>(1.0 / publish_frequency), std::bind(&MinimalPublisher::timer_callback, this));
+    service_ = this->create_service<cpp_pubsub::srv::ModifyMessage>(
+    "modify_message", std::bind(&MinimalPublisher::modifyMessageCallback, this, _1, _2)
+  );
+
+   // Use DEBUG logging level
+    RCLCPP_DEBUG_STREAM(this->get_logger(), "Debug message in constructor");
+
+    // Use INFO logging level
+    RCLCPP_INFO_STREAM(this->get_logger(), "Node initialized");
+
+    // Use WARN logging level
+    RCLCPP_WARN_STREAM(this->get_logger(), "Warning message in constructor");
+
+    // Use ERROR logging level
+    RCLCPP_ERROR_STREAM(this->get_logger(), "Error message in constructor");
+
+    // Use FATAL logging level
+    RCLCPP_FATAL_STREAM(this->get_logger(), "Fatal error message in constructor");
+
   }
 
 private:
@@ -44,15 +68,51 @@ private:
     RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
     publisher_->publish(message);
   }
+
+  bool modifyMessageCallback(
+  const std::shared_ptr<cpp_pubsub::srv::ModifyMessage::Request> request,
+  std::shared_ptr<cpp_pubsub::srv::ModifyMessage::Response> response
+)
+{
+  auto message = std_msgs::msg::String();
+  message.data = request->new_message;
+
+  // Use DEBUG logging level
+  RCLCPP_DEBUG_STREAM(this->get_logger(), "Debug message in modifyMessageCallback");
+
+  // Use ERROR logging level
+  RCLCPP_ERROR_STREAM(this->get_logger(), "Error message in modifyMessageCallback");
+
+  // Use FATAL logging level
+  RCLCPP_FATAL_STREAM(this->get_logger(), "Fatal error message in modifyMessageCallback");
+
+  // Use INFO logging level
+  RCLCPP_INFO_STREAM(this->get_logger(), "Message modified: '" << message.data << "'");
+
+  publisher_->publish(message);
+
+  response->success = true;
+  return true;
+}
+
+
   rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+  rclcpp::Service<cpp_pubsub::srv::ModifyMessage>::SharedPtr service_;
   size_t count_;
 };
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
+  // Use INFO logging level
+  RCLCPP_INFO(rclcpp::get_logger("minimal_publisher_main"), "Starting MinimalPublisher node");
+
   rclcpp::spin(std::make_shared<MinimalPublisher>());
+
+  // Use INFO logging level
+  RCLCPP_INFO(rclcpp::get_logger("minimal_publisher_main"), "Shutting down MinimalPublisher node");
+
   rclcpp::shutdown();
   return 0;
 }
